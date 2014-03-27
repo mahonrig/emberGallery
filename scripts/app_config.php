507aece68fb4b51979c77771407662a7ddd19276
Mahonri Gibson <mahonrig@gmail.com>
2014-03-27 16:27:24 -0700

    File check was messing up setup process.

--- scripts/app_config.php
+++ scripts/app_config.php
@@ -1,9 +1,6 @@
 <?php
 	// Database connection
-	if (!is_file("scripts/config.ini")) {
-       header("Location: /scripts/setup.html");
-    }
-	$db_config = parse_ini_file("scripts/config.ini");
+	$db_config = parse_ini_file("config.ini");
 	define('DATABASE_HOST', $db_config['db_host']);	
 	define('DATABASE_USERNAME', $db_config['db_username']);	
 	define('DATABASE_PASSWORD', $db_config['db_password']);	
