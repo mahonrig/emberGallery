055beebf5c322c1fd406609d94dc0e88ab1d5736
Mahonri Gibson <mahonrig@gmail.com>
2014-03-26 21:56:29 -0700

    Views for drag and drop upload, with shadow box photo preview.

--- js/views.js
+++ js/views.js
@@ -9,18 +9,20 @@ Photoworks.GalleriesView = Ember.View.extend({
 Photoworks.GalleryView = Ember.View.extend({
     templateName: 'gallery',
     didInsertElement: function() {
-        this.get('controller').send('hideGalleries');
-        this.get('controller').send('showPhotos');
-        $('title').text('Viewing Gallery - ' + this.get('controller').get('model.title'));
+        var controller = this.get('controller');
+        controller.send('hideGalleries');
+        controller.send('showPhotos');
+        $('title').text('Viewing Gallery - ' + controller.get('model.title'));
     }
 });
 
 Photoworks.PhotoView = Ember.View.extend({
     templateName: 'photo',
     didInsertElement: function() {
-        this.get('controller').send('hideGalleries');
-        this.get('controller').send('hidePhotos');
-        $('title').text(this.get('controller').get('model.title'));
+        var controller = this.get('controller');
+        controller.send('hideGalleries');
+        controller.send('hidePhotos');
+        $('title').text(controller.get('model.title'));
     }
 });
 
@@ -28,20 +30,20 @@ Photoworks.OrderView = Ember.View.extend({
     templateName: 'order',
     didInsertElement: function(){
         imagesLoaded( '.productLarge', function() {
-            var $largeImage = $('.largeImage');
-            var imgWidth = $largeImage.width();
-            var imgHeight = $largeImage.height();
+            var $img = $('.largeImage');
+            var imgWidth = $img.width();
+            var imgHeight = $img.height();
             var windowWidth = $( window ).width();
             var windowHeight = $( window ).height();
             if (imgWidth >= imgHeight){
-                $largeImage.width(windowWidth * .9);
-                if ($largeImage.height() > windowHeight){
-                    var factor = (windowHeight * .8) / $largeImage.height();
-                    $largeImage.height($largeImage.height() * factor);
-                    $largeImage.width($largeImage.width() * factor);
+                $img.width(windowWidth * .9);
+                if ($img.height() > windowHeight){
+                    var factor = (windowHeight * .8) / $img.height();
+                    $img.height($img.height() * factor);
+                    $img.width($img.width() * factor);
                 }
             } else {
-                $largeImage.height(windowHeight * .8);
+                $img.height(windowHeight * .8);
             }
         });
     }
@@ -49,24 +51,33 @@ Photoworks.OrderView = Ember.View.extend({
 
 Photoworks.EditTitleTextView = Ember.TextField.extend({
     didInsertElement: function() {
+        /* Set input element slightly larger than the text */
         this.$().attr('size', this.$().val().length * 1.1);
         this.$().focus();
     },
+
+    /* For some reason this doesn't save everytime, only usually */
     focusOut: function(){
         this.get('parentView').set('titleEditing', false);
         this.get('parentView').send('acceptTitleChange');
     },
-    
+
     insertNewline: function(evt){
+        /* This triggers the focusOut event, which saves the model */
         this.get('parentView').set('titleEditing', false);
-        this.get('parentView').send('acceptTitleChange');
+    },
+
+    click: function(evt){
+        evt.preventDefault();
     }
 });
+
+Ember.Handlebars.helper('edit-title', Photoworks.EditTitleTextView);
 
 Photoworks.EditTitleView = Ember.View.extend({
     templateName: 'editTitle',
     titleEditing: false,
-    
+
     actions: {
         editTitle: function(){
             this.set('titleEditing', true);
@@ -74,8 +85,115 @@ Photoworks.EditTitleView = Ember.View.extend({
         acceptTitleChange: function(){
             this.get('controller').send('acceptTitleChange');
         }
-        
+
     }
 });
 
-Ember.Handlebars.helper('edit-title', Photoworks.EditTitleTextView);
+Photoworks.UploadPhotosView = Ember.View.extend({
+        templateName: 'addPhotos',
+
+        /* drag enter/over need to be set to prevent exploding
+         * will add css effects here in future */
+        dragEnter: function(event) {
+            event.preventDefault();
+        },
+
+        dragOver: function(event) {
+            event.preventDefault();
+        },
+
+        drop: function(event) {
+            event.preventDefault();
+
+            /* Loop through each dropped photo, create new view for the images */
+            var files = event.dataTransfer.files;
+            for (var i = 0; i < files.length; i++){
+                //console.log(files[i]);
+                if (files[i].type.match("image.*")) {
+                  /* Push a new view representing our photo to the container within the shadowbox */
+                  Photoworks.NewPhotosContainer.pushObject(Photoworks.NewPhotosView.create({
+                    file: files[i],
+                  }));
+                }
+            }
+
+            /* shadow box should now hold the new photos, lets show it */
+            $('.shadowBox').width($(window).width());
+            $('.shadowBox').height($(window).height());
+            $('.shadowBox').fadeIn();
+        },
+});
+
+/* Inside shadow box in addPhotos template,
+ * holds each of the NewPhotosView */
+Photoworks.NewPhotosContainer = Ember.ContainerView.create({
+    childViews: [],
+});
+
+/* Used for each of our new photos to submit
+ * image source is set in the drop function on create */
+Photoworks.NewPhotosView = Ember.View.extend({
+    templateName: 'newPhotoDetails',
+    title: 'Untitled', /* Default title */
+    src: '', /* Image preview */
+    /* file: is set upon create during drop event */
+    /* For file preview, get the image data */
+    setsrc: function(){
+      var that = this;
+      var reader = new FileReader();
+      reader.readAsDataURL(this.get('file'));
+      reader.onload = function(imgsrc){
+        that.set('src', imgsrc.target.result);
+      }
+    }.observes('file').on('init'),
+
+    actions: {
+
+        /* fade out / remove photo, if lightbox is empty, fade it out */
+        cancel: function(){
+            var that = this;
+            this.$().fadeOut(function(){
+                Photoworks.NewPhotosContainer.removeObject(that);
+                if ($('.shadowBox>.ember-view').html() == ''){
+                    $('.shadowBox').fadeOut();
+                }
+            });
+
+
+        },
+
+        /* Upload photo to server, which resizes and saves
+         * Should resize these photos to some max size or under
+         * and then upload. Need to refresh model still */
+        submit: function(){
+          var that = this;
+          var formData = new FormData();
+          formData.append('file', this.get('file'));
+          formData.append('title', this.get('title'));
+          xhr = new XMLHttpRequest();
+          xhr.open('POST', '/photos/new');
+
+          /* Haven't checked to see if this works yet */
+          xhr.upload.onprogress = function (event) {
+            if (event.lengthComputable) {
+              var complete = (event.loaded / event.total * 100 | 0);
+              that.$('progress').value = complete;
+            }
+          }
+
+          xhr.onload = function() {
+            /* Remove our view object, fade out shadow box if empty */
+            Photoworks.NewPhotosContainer.removeObject(that);
+            if ($('.shadowBox>.ember-view').html() == ''){
+                $('.shadowBox').fadeOut();
+            }
+
+          }
+
+          /* Send out our request */
+          xhr.send(formData);
+
+        }
+    }
+
+});
